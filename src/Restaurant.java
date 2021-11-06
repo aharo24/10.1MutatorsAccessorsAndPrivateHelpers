08ae4055bd296394294bf5501dc408d2f81baae8
ItachiCodes <52618896+ItachiCodes@users.noreply.github.com>
2021-11-06 01:03:35 -0700

    Initial commit

---
+++ src/Restaurant.java
@@ -0,0 +1,24 @@
+public class Restaurant {                          
+   private String name;
+   private int rating;
+
+   public void setName(String restaurantName) {  // Mutator
+      name = restaurantName;
+   }
+
+   public void setRating(int userRating) {       // Mutator
+      rating = userRating;
+   }
+
+   public String getName() {  // Accessor
+      return name;
+   }
+
+   public int getRating() {  // Accessor
+      return rating;
+   }
+
+   public void print() {      // Accessor
+      System.out.println(name + " -- " + rating);
+   }
+}
